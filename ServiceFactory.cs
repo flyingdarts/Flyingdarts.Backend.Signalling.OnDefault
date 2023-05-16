95340a02d37839cc86b451352a279438da7b9f92
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-16 23:55:39 +0200

    add an IAmazonDynamoDb service instance

--- ServiceFactory.cs
+++ ServiceFactory.cs
@@ -8,7 +8,7 @@ public static class ServiceFactory
     public static ServiceProvider GetServiceProvider()
     {
         var services = new ServiceCollection();
-        services.AddSingleton<AmazonDynamoDBClient>();
+        services.AddAWSService<IAmazonDynamoDB>();
         services.AddValidatorsFromAssemblyContaining<OnDefaultCommandValidator>();
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(OnDefaultCommand).Assembly));
         return services.BuildServiceProvider();
