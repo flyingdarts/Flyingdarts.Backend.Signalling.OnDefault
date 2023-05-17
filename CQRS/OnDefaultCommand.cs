5e32a424b7b83ccfeda6e400e77e4ff7567870b7
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-17 11:26:39 +0200

    Add properties to command

--- CQRS/OnDefaultCommand.cs
+++ CQRS/OnDefaultCommand.cs
@@ -2,6 +2,9 @@
 {
     public class OnDefaultCommand : IRequest<APIGatewayProxyResponse>
     {
+        public string Message { get; set; }
+        public Guid Owner { get; set; }
+        public string Date { get; set; }
         public string ConnectionId { get; set; }
         internal AmazonApiGatewayManagementApiClient ApiGatewayManagementApiClient { get; set; }
     }
